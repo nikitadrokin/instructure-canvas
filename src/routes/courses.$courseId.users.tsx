5e290e7668cccfbaa0112a91b9153e53f0b11e63
Nikita D <dnawastaken@gmail.com>
2026-09-09 23:30:43 -0700

    Add searchable course people page

---
+++ src/routes/courses.$courseId.users.tsx
@@ -0,0 +1,4 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { CourseSection } from "@/components/courses/course-section";
+export const Route = createFileRoute("/courses/$courseId/users")({ component: Page });
+function Page() { const { courseId } = Route.useParams(); return <CourseSection courseId={courseId} section="people" />; }
