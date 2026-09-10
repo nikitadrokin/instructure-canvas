08dec8e4a3a95ca006d5cdc16457086fe3260fad
Nikita D <dnawastaken@gmail.com>
2026-09-09 23:30:43 -0700

    Add personal course grades page

---
+++ src/routes/courses.$courseId.grades.tsx
@@ -0,0 +1,4 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { CourseSection } from "@/components/courses/course-section";
+export const Route = createFileRoute("/courses/$courseId/grades")({ component: Page });
+function Page() { const { courseId } = Route.useParams(); return <CourseSection courseId={courseId} section="grades" />; }
